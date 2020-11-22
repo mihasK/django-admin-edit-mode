4eb1c78c1f8385759a8da6a63bbd52932fd3b0e7
mihas <mkoypish@gmail.com>
2020-11-22 23:10:33 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='Django Admin Edit Mode',
    version='0.1.0',
    author='Mikhail Koipish',
    author_email='mkoypish@gmail.com',
-   packages=[],
+   packages=find_packages(),
    scripts=[],
    url='http://pypi.python.org/pypi/PackageName/',
    license='LICENSE.txt',
